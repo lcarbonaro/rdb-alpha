bcc811ef2705b05ac94762c18b04bd88b4a6e183
Les Carbonaro <lcarbonaro@gmail.com>
2026-01-19 10:22:55 -0500

    update sql file

--- salon.sql
+++ salon.sql
@@ -173,12 +173,15 @@ ALTER TABLE ONLY public.services ALTER COLUMN service_id SET DEFAULT nextval('pu
 -- Data for Name: appointments; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.appointments VALUES (8, 11, 1, '10:30');
+INSERT INTO public.appointments VALUES (9, 11, 2, '11am');
 
 
 --
 -- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.customers VALUES (11, '555-555-5555', 'Fabio');
 
 
 --
@@ -194,14 +197,14 @@ INSERT INTO public.services VALUES (3, 'PEDICURE');
 -- Name: appointments_appointment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.appointments_appointment_id_seq', 1, false);
+SELECT pg_catalog.setval('public.appointments_appointment_id_seq', 14, true);
 
 
 --
 -- Name: customers_customer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.customers_customer_id_seq', 1, false);
+SELECT pg_catalog.setval('public.customers_customer_id_seq', 15, true);
 
 
 --
